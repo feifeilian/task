2e57212939c30c6e9d49f5ac4fd1b8c5c489b691
feifeilian <291970537@qq.com>
2017-07-07 22:16:41 +0800

    任务29

---
+++ task4/data.js
@@ -0,0 +1,511 @@
+  var menus = {
+      "1": {
+          "pid": "1",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "2": {
+          "pid": "2",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "3": {
+          "pid": "3",
+          "name": "evel1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "4": {
+          "pid": "4",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "5": {
+          "pid": "5",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "6": {
+          "pid": "6",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "7": {
+          "pid": "7",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "8": {
+          "pid": "8",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "9": {
+          "pid": "9",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "10": {
+          "pid": "10",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "11": {
+          "pid": "11",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "12": {
+          "pid": "12",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "13": {
+          "pid": "13",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "14": {
+          "pid": "14",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "15": {
+          "pid": "15",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "16": {
+          "pid": "16",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "17": {
+          "pid": "17",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "18": {
+          "pid": "18",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "19": {
+          "pid": "19",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "20": {
+          "pid": "20",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "21": {
+          "pid": "21",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "22": {
+          "pid": "22",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "23": {
+          "pid": "23",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "24": {
+          "pid": "24",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "25": {
+          "pid": "25",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "26": {
+          "pid": "26",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "27": {
+          "pid": "27",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "28": {
+          "pid": "28",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "29": {
+          "pid": "29",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+      "30": {
+          "pid": "30",
+          "name": "level1 Some Text",
+          "sub_menus": [{
+              "name": "level2 Some Text",
+          }, {
+              "name": "level2 Some Text",
+          }]
+
+      },
+
+  }
+
+var tabledata = {
+    "0": {
+        "id": "0",
+        "name": "姓名",
+        "content": "城市",
+        "value": "身高",
+        "operate": "操作"
+    },
+    "1": {
+        "id": "1",
+        "name": "zs",
+        "content": "天津",
+        "value": "176"
+    },
+    "2": {
+        "id": "2",
+        "name": "ls",
+        "content": "北京",
+        "value": "175"
+    },
+    "3": {
+        "id": "3",
+        "name": "lm",
+        "content": "哈尔滨",
+        "value": "180"
+    },
+    "4": {
+        "id": "4",
+        "name": "zs",
+        "content": "厦门",
+        "value": "170"
+    },
+    "5": {
+        "id": "5",
+        "name": "zs",
+        "content": "天津",
+        "value": "176"
+    },
+    "6": {
+        "id": "6",
+        "name": "ls",
+        "content": "北京",
+        "value": "175"
+    },
+    "7": {
+        "id": "7",
+        "name": "lm",
+        "content": "哈尔滨",
+        "value": "180"
+    },
+    "8": {
+        "id": "8",
+        "name": "zs",
+        "content": "厦门",
+        "value": "170"
+    },
+
+    "9": {
+        "id": "9",
+        "name": "zs",
+        "content": "天津",
+        "value": "176"
+    },
+    "10": {
+        "id": "10",
+        "name": "ls",
+        "content": "北京",
+        "value": "175"
+    },
+    "11": {
+        "id": "11",
+        "name": "lm",
+        "content": "哈尔滨",
+        "value": "180"
+    },
+    "12": {
+        "id": "12",
+        "name": "zs",
+        "content": "厦门",
+        "value": "170"
+    },
+
+    "13": {
+        "id": "13",
+        "name": "zs",
+        "content": "天津",
+        "value": "176"
+    },
+    "14": {
+        "id": "14",
+        "name": "ls",
+        "content": "北京",
+        "value": "175"
+    },
+    "15": {
+        "id": "15",
+        "name": "lm",
+        "content": "哈尔滨",
+        "value": "180"
+    },
+    "16": {
+        "id": "16",
+        "name": "zs",
+        "content": "厦门",
+        "value": "170"
+    },
+
+    "17": {
+        "id": "17",
+        "name": "zs",
+        "content": "天津",
+        "value": "176"
+    },
+    "18": {
+        "id": "18",
+        "name": "ls",
+        "content": "北京",
+        "value": "175"
+    },
+    "19": {
+        "id": "19",
+        "name": "lm",
+        "content": "哈尔滨",
+        "value": "180"
+    },
+    "20": {
+        "id": "20",
+        "name": "zs",
+        "content": "厦门",
+        "value": "170"
+    },
+
+    "21": {
+        "id": "21",
+        "name": "zs",
+        "content": "天津",
+        "value": "176"
+    },
+    "22": {
+        "id": "22",
+        "name": "ls",
+        "content": "北京",
+        "value": "175"
+    },
+    "23": {
+        "id": "23",
+        "name": "lm",
+        "content": "哈尔滨",
+        "value": "180"
+    },
+    "24": {
+        "id": "24",
+        "name": "zs",
+        "content": "厦门",
+        "value": "170"
+    },
+
+    "25": {
+        "id": "25",
+        "name": "zs",
+        "content": "天津",
+        "value": "176"
+    },
+    "26": {
+        "id": "26",
+        "name": "ls",
+        "content": "北京",
+        "value": "175"
+    },
+    "27": {
+        "id": "27",
+        "name": "lm",
+        "content": "哈尔滨",
+        "value": "180"
+    },
+    "28": {
+        "id": "28",
+        "name": "zs",
+        "content": "厦门",
+        "value": "170"
+    },
+    "29": {
+        "id": "29",
+        "name": "zs",
+        "content": "天津",
+        "value": "176"
+    },
+    "30": {
+        "id": "30",
+        "name": "ls",
+        "content": "北京",
+        "value": "175"
+    },
+    "31": {
+        "id": "31",
+        "name": "lm",
+        "content": "哈尔滨",
+        "value": "180"
+    },
+    "32": {
+        "id": "32",
+        "name": "zs",
+        "content": "厦门",
+        "value": "170"
+    },
+
+}
